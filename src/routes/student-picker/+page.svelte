0dea389becf84aa9ad14db1c3038720481d40ac3
rogerwang2008 <rogerwang2008@163.com>
2025-03-24 21:48:50 +0800

    新增 实现点名器的最基本界面

---
+++ src/routes/student-picker/+page.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+  import StudentCard from "./StudentCard.svelte";
+  import { onMount, onDestroy } from "svelte";
+
+  let mainElement: HTMLElement;
+  let studentCardFatherElement: HTMLElement;
+  let studentCardFatherZoom: number = $state(1);
+
+  let studentCardFatherObserver: ResizeObserver;
+
+  onMount(() => {
+    if (mainElement && studentCardFatherElement) {
+      studentCardFatherObserver = new ResizeObserver((entries) => {
+        for (let entry of entries) {
+          if (!entry.contentBoxSize) {continue;}
+          console.log(mainElement.clientWidth, studentCardFatherElement.clientWidth)
+          if (mainElement.clientWidth / mainElement.clientHeight > studentCardFatherElement.clientWidth / studentCardFatherElement.clientHeight) {
+            studentCardFatherZoom = mainElement.clientHeight / studentCardFatherElement.clientHeight;
+          } else {
+            studentCardFatherZoom = mainElement.clientWidth / studentCardFatherElement.clientWidth;
+          }
+        }
+      });
+      studentCardFatherObserver.observe(mainElement);
+    }
+  });
+
+  onDestroy(() => {
+      studentCardFatherObserver.disconnect();
+    },
+  );
+
+</script>
+
+<main bind:this={mainElement} class="flex size-[98%] mx-auto mb-[2%]">
+  <div bind:this={studentCardFatherElement}
+       class="inline-grid grid-cols-[repeat(8,minmax(min-content,max-content))] gap-2 m-auto w-auto"
+       style="zoom: {studentCardFatherZoom}"
+  >
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+    <StudentCard studentId={1} studentName="张某某"></StudentCard>
+
+  </div>
+
+</main>
---
+++ src/routes/student-picker/StudentCard.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  interface Props {
+    studentId: number,
+    studentName: string,
+    state?: boolean | "disabled" | "unavailable",
+  }
+
+  let { studentId, studentName, state = false }: Props = $props();
+</script>
+
+<div
+  class='card w-32 h-24 items-center justify-center select-none
+  {state === true ? "bg-success text-success-content"
+    : state === "disabled" ? "bg-base-300 text-base-content"
+    : state === "unavailable" ? "bg-base-100 text-base-content/80"
+    : "bg-info text-info-content"}'>
+  <span class="text-3xl">{studentId}</span>
+  <span class="text-xl">{studentName}</span>
+</div>
+
